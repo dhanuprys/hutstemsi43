a33dd95a826c63944c16008eeac7bf3f434a4d32
dhanuprys <dhanuprys@gmail.com>
2023-05-31 22:31:10 +0800

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import Header from '../components/Header';
 import LandingPage from '../components/LandingPage';
 import styles from './page.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'HUT STEMSI 43 | EUFORIA 43',
+  openGraph: {
+    title: 'HUT STEMSI 43 | EUFORIA 43',
+    description: 'Meriahkan HUT STEMSI yang ke-43 dengan ikut berpartisipasi dalam lomba-lomba yang diselenggarakan',
+    url: 'https://hutstemsi43.vercel.app',
+    siteName: 'HUT STEMSI 43',
+    images: [
+      {
+        url: 'https://hutstemsi43.vercel.app/logo.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://hutstemsi43.vercel.app/logo.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  }
+};
 
 export default function Home() {
   return (
     <>
       <Header current="home" />
       <LandingPage />
-      <footer>
-
-      </footer>
     </>
   )
 }
--- src/app/syarat-dan-ketentuan/page.tsx
+++ src/app/syarat-dan-ketentuan/page.tsx
@@ -1,5 +1,31 @@
 import Header from '@/components/Header';
 import TermsAndCondition from '@/components/TermsAndConditions';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'HUT STEMSI 43 | EUFORIA 43',
+  openGraph: {
+    title: 'HUT STEMSI 43 | EUFORIA 43',
+    description: 'Berikut merupakan syarat & ketentuan lomba yang diselenggarakan',
+    url: 'https://hutstemsi43.vercel.app/syarat-dan-ketentuan',
+    siteName: 'HUT STEMSI 43',
+    images: [
+      {
+        url: 'https://hutstemsi43.vercel.app/logo.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://hutstemsi43.vercel.app/logo.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  }
+};
 
 export default function Ketentuan({ params }: { params: { lomba: string } }) {
   return (
