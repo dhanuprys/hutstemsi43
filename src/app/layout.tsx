29b14f56a8e6f402882587aa814e5211a46e517e
dhanuprys <dhanuprys@gmail.com>
2023-06-04 18:12:13 +0800

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
-'use client';
-
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
 import './globals.css'
 import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] });\
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
